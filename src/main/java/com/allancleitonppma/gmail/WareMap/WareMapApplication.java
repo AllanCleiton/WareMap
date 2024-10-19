6a02f46dc032a1d1066ee3c72a02e8380371bafc
Allan Cleiton <allancleitonppma@gmail.com>
2024-10-19 15:27:53 -0300

    Project Created _empty.

---
+++ src/main/java/com/allancleitonppma/gmail/WareMap/WareMapApplication.java
@@ -0,0 +1,13 @@
+package com.allancleitonppma.gmail.WareMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WareMapApplication.class, args);
+	}
+
+}
